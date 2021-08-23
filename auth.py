b28d468f1b3b65c4b9ce119027bbb7e54f12ee7c
ImBUNER <rbns71@gmail.com>
2021-08-23 14:08:38 +0200

    v.1.1 intento de esquema register con orm

--- auth.py
+++ auth.py
@@ -8,7 +8,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import redirect
 
 from . import db
-from .models import user
+from .models import personal, user
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -19,16 +19,33 @@ def register():
         tim = request.form['tim']
         password = request.form['password']
         password_2 = request.form['password_repeat']
+        error = None
         
+        correo_exists = personal.query.filter_by(correo=correo).first()
+        tim_exists = personal.query.filter_by(tim=tim).first()
+        user_exists = user.query.filter_by(id=tim_exists['id']).first()
+
+        if not correo:
+            error = 'Correo corporativo es requerido'
+        elif not tim:
+            error = 'TIM es requerida'
+        elif not password:
+            error = 'Contraseña es requerida'
+        elif correo_exists is None:
+            error = 'El correo corporativo "{}" no consta en la base de datos de la unidad.'.format(correo)
+        elif tim_exists is None:
+            error = 'La TIM "{}" no consta en la base de datos de la unidad.'.format(tim)
+        elif user_exists:
+            error = 'Ya existe un usuario registrado que está enlazado a ese correo y esa TIM.'
+        elif password != password_2:
+            error = 'Las contraseñas no coinciden.'
         
-
-
-        new_user = user(password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-
-        flash(error)
+        if error is None:
+            new_user = user(password=generate_password_hash(password), id_personal=tim_exists['id'])
+            db.session.add(new_user)
+            db.session.commit()
+            flash(error)
+            return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html')
 
